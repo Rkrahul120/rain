6981dd56d46bebd8d60caad39ceba2f55393cea4
Rkrahul120 <satish2020@gift.edu.in>
2024-12-20 00:38:18 +0530

    Add files via upload
    hdhq

---
+++ script.js
@@ -0,0 +1,10 @@
+function Rain(){
+    let vid=document.getElementById("Vid")
+    vid.src="rain.mp4"
+    let aud=document.getElementById("audio")
+    aud.src="ringtone.mp3"
+}
+function NoRain(){
+    let vid=document.getElementById("Vid")
+    vid.src="norain.mp4"
+}
